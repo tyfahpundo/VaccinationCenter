5c17f9c72aa2527052975c8d1a07ad4455cbee11
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-10-04 18:58:38 +0200

    Initial Commit

---
+++ src/main/java/zw/co/afrosoft/vaccinationcenter/VaccinationCenterApplication.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft.vaccinationcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccinationCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zw/co/afrosoft/vaccinationcenter/VaccinationCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft.vaccinationcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
